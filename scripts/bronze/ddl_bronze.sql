018adcbf9387c9553c910e6438fad9bf76e0aea5
beneisenman <beneisenman@gmail.com>
2026-02-07 12:52:34 -0800

    ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,25 +13,25 @@ IF OBJECT_ID ('bronze.crm_cust_info' , 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 
 CREATE TABLE bronze.crm_cust_info (
-	cst_id 					    INT,
-	cst_key 				    NVARCHAR(50),
-	cst_firstname 			NVARCHAR(50),
-	cst_lastname 			  NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
-	cst_gndr 				    NVARCHAR(50),
-	cst_create_date 		DATE
+	cst_id 				INT,
+	cst_key 			NVARCHAR(50),
+	cst_firstname 		NVARCHAR(50),
+	cst_lastname 		NVARCHAR(50),
+	cst_maritial_status NVARCHAR(50),
+	cst_gndr 			NVARCHAR(50),
+	cst_create_date 	DATE
 );
 
 IF OBJECT_ID ('bronze.crm_prd_info' , 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info (
-	prd_id 			  INT,
-	prd_key 		  NVARCHAR(50),
-	prd_nm 			  NVARCHAR(50),
-	prd_cost 		  INT,
-	prd_line 		  NVARCHAR(50),
-	prd_start_dt  DATETIME,
+	prd_id 			INT,
+	prd_key 		NVARCHAR(50),
+	prd_nm 			NVARCHAR(50),
+	prd_cost 		INT,
+	prd_line 		NVARCHAR(50),
+	prd_start_dt  	DATETIME,
 	prd_end_dt 		DATETIME
 );
 
@@ -54,25 +54,25 @@ IF OBJECT_ID ('bronze.erp_loc_a101' , 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 
 CREATE TABLE bronze.erp_loc_a101 (
-	cid 	NVARCHAR(50),
-	cntry NVARCHAR(50)
+	cid		NVARCHAR(50),
+	cntry 	NVARCHAR(50)
 );
 
 IF OBJECT_ID ('bronze.erp_cust_az12' , 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 
 CREATE TABLE bronze.erp_cust_az12 (
-	cid 	  NVARCHAR(50),
+	cid 	NVARCHAR(50),
 	bdate 	DATE,
-	gen 	  NVARCHAR(50)
+	gen 	NVARCHAR(50)
 );
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-	id 			    NVARCHAR(50),
-	cat 		    NVARCHAR (50),
-	subcat 		  NVARCHAR(50),
+	id 			NVARCHAR(50),
+	cat 		NVARCHAR (50),
+	subcat 		NVARCHAR(50),
 	maintenance NVARCHAR(50)
 );
