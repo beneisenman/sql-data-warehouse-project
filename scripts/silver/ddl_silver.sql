e88e61da3ca3013f31ea0e61d15f40ff68fbbaa3
beneisenman <beneisenman@gmail.com>
2026-02-07 17:24:31 -0800

    ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -14,12 +14,12 @@ IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 
 CREATE TABLE silver.crm_cust_info (
-	cst_id 					    INT,
-	cst_key 				    NVARCHAR(50),
+	cst_id 					INT,
+	cst_key 				NVARCHAR(50),
 	cst_firstname 			NVARCHAR(50),
-	cst_lastname 			  NVARCHAR(50),
-	cst_marital_status 	NVARCHAR(50),
-	cst_gndr 				    NVARCHAR(50),
+	cst_lastname 			NVARCHAR(50),
+	cst_marital_status 		NVARCHAR(50),
+	cst_gndr 				NVARCHAR(50),
 	cst_create_date 		DATE,
 	dwh_create_date			DATETIME2 DEFAULT GETDATE()
 );
@@ -34,9 +34,9 @@ CREATE TABLE silver.crm_prd_info (
 	prd_nm 			    NVARCHAR(50),
 	prd_cost 		    INT,
 	prd_line 		    NVARCHAR(50),
-	prd_start_dt 	  DATE,
-	prd_end_dt 		  DATE,
-	dwh_create_date	DATETIME2 DEFAULT GETDATE()
+	prd_start_dt 	  	DATE,
+	prd_end_dt 		  	DATE,
+	dwh_create_date		DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.crm_sales_details' , 'U') IS NOT NULL
@@ -59,28 +59,28 @@ IF OBJECT_ID ('silver.erp_loc_a101' , 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 
 CREATE TABLE silver.erp_loc_a101 (
-	cid 			      NVARCHAR(50),
+	cid 			  	NVARCHAR(50),
 	cntry 			    NVARCHAR(50),
-	dwh_create_date	DATETIME2 DEFAULT GETDATE()
+	dwh_create_date		DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.erp_cust_az12' , 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
 
 CREATE TABLE silver.erp_cust_az12 (
-	cid 			      NVARCHAR(50),
+	cid 			 	NVARCHAR(50),
 	bdate 		     	DATE,
-	gen 			      NVARCHAR(50),
-	dwh_create_date	DATETIME2 DEFAULT GETDATE()
+	gen 			  	NVARCHAR(50),
+	dwh_create_date		DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.erp_px_cat_g1v2' , 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
-	id 				      NVARCHAR(50),
-	cat 			      NVARCHAR (50),
+	id 				    NVARCHAR(50),
+	cat 			    NVARCHAR (50),
 	subcat 			    NVARCHAR(50),
-	maintenance 	  NVARCHAR(50),
-	dwh_create_date	DATETIME2 DEFAULT GETDATE()
+	maintenance 	  	NVARCHAR(50),
+	dwh_create_date		DATETIME2 DEFAULT GETDATE()
 );
